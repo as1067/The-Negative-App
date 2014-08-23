796fd1edc947288c20451b189649a012e86d121e
Cyrus Tau <cyrus.tau@gmail.com>
2014-08-22 20:51:07 -0700

    Initial Commit

---
+++ The Negative AppTests/The_Negative_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  The_Negative_AppTests.swift
+//  The Negative AppTests
+//
+//  Created by Monika Gorkani on 8/22/14.
+//  Copyright (c) 2014 Cyrus Tau Inc. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class The_Negative_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
